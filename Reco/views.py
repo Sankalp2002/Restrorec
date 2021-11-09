9de59d8d78d6b0890c91e998320fece2825847f9
Sankalp <sankalprajendran@gmail.com>
2021-11-09 19:21:14 +0530

    update

--- Reco/views.py
+++ Reco/views.py
@@ -23,6 +23,23 @@ from django.core.validators import ValidationError,validate_email
 # from django.conf import settings
 # from settings import STATIC_DIR
 # Create your views here.
+def initial():
+    df_item= pd.read_csv('/static/datasets/all_items.csv')
+    df_rest = pd.read_csv('/static/datasets/all_rest.csv')
+    df_rest.columns = ['Name', 'Rating','Cuisine', 'Address', 'No. of Ratings']
+    rest_list=df_rest.values.tolist()
+    item_list=df_item.values.tolist()
+
+    for row in rest_list:
+        Restaurant.objects.get_or_create(name=row[0],rating=row[1],cuisine=row[2],address=row[3],totalRatings=row[4])
+
+    for row in item_list:
+        t=row[6]
+        if t<24:
+            menuItem.objects.get_or_create(category=row[0],name=row[1],price=row[2],description=row[3],diet=row[4],rating=row[5],restaurantId_id=91+t)
+        else:
+            menuItem.objects.get_or_create(category=row[0],name=row[1],price=row[2],description=row[3],diet=row[4],rating=row[5],restaurantId_id=90+t)
+
 def foodfun(fname):
     df= pd.read_csv('../RESTROREC/static/datasets/indian_food2.csv')
     food=[fname,-1,-1,-1,-1,-1]
